14f2410440e4f14f076e383d4333435a16b81a66
Javier Santoyo <javier.santoyo@yahoo.com>
2024-03-25 15:35:30 -0600

    feat: :sparkles: Update background image on Error404 page

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -1,6 +1,24 @@
+import styled from "styled-components";
+import error404 from "./404Background.webp";
 
 export const Error404 = () => {
+  const handleClickBack = () => {
+    window.history.back();
+  };
+
   return (
-    <div>Error404</div>
-  )
-}
+    <ErrorBody onClick={handleClickBack} style={{ cursor: "pointer" }}>
+      <Logo src={error404} alt="Logo" />
+    </ErrorBody>
+  );
+};
+
+const ErrorBody = styled.div`
+  display: flex;
+  overflow-x: hidden;
+`;
+
+const Logo = styled.img`
+  width: 100vw;
+  height: 100vh;
+`;
