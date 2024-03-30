528f04c56a210d654b6136449165ac3b836a5f7a
Jose Montenegro <grafijoss@hotmail.com>
2024-03-30 18:31:31 -0500

    fetch hook

---
+++ src/hooks/useGetRecipes.tsx
@@ -0,0 +1,30 @@
+
+import { useEffect, useState } from 'react';
+import { useRestClient } from './useRestClient';
+import type { Recipe } from '../types/recipes';
+import { recipesMap } from '../mappers/recipeMap'
+
+export function useGetRecipesService(url: string): {
+    recipes: Recipe[], 
+    loading: boolean, 
+    error: unknown
+} {
+  const [get, rawData, loading, error] = useRestClient('get');
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+        await get(url);
+    }
+    
+    fetchData();
+  }, [url, get])
+
+  useEffect(() => {
+    if (Array.isArray(rawData?.meals) && rawData.meals.length > 0) {
+        setRecipes(recipesMap(rawData.meals))
+    }
+  }, [rawData]);
+
+  return { recipes, loading, error };
+}
---
+++ src/hooks/useRestClient.tsx
@@ -0,0 +1,38 @@
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+
+type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+export type UseAxiosFunction = (url: string, options?: AxiosRequestConfig) => Promise<AxiosResponse<unknown>>;
+
+export function useRestClient(method: HttpMethod): [
+    UseAxiosFunction, 
+    Record<string, unknown>, 
+    boolean, 
+    unknown
+  ] {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<unknown>(null);
+
+  const request = useCallback(
+    async (url: string, options?: AxiosRequestConfig) => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await axios({ method, url, ...options });
+        setLoading(false);
+        setData(response.data|| {})
+        return response;
+      } catch (err) {
+        setLoading(false);
+        setError(err);
+        throw err;
+      }
+    },
+    [method]
+  );
+
+  return [request, data, loading, error];
+}
