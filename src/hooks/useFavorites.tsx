cb82204e54d716ebd7adfb4a94873d08a6d58477
Jose Montenegro <grafijoss@hotmail.com>
2024-04-05 22:07:23 -0500

    add favorites hook

---
+++ src/hooks/useFavorites.tsx
@@ -0,0 +1,36 @@
+import useStore from '../store/favorites';
+import type { Recipe } from '../types/recipes';
+
+const useFavorites = () => {
+    const favorites = useStore(state => state.favorites);
+    const setFavorites = useStore(state => state.setFavorites);
+
+    const toogleFavorites = (newFavorite: Recipe) => {
+        let updatedFavorites = [];
+        const foundObject = favorites.find(obj => obj.id === newFavorite.id);
+
+        if (foundObject) {
+            updatedFavorites = favorites.filter(favorite => favorite.id !== newFavorite.id);
+        } else {
+            updatedFavorites = [...favorites, newFavorite];
+        }
+
+        setFavorites(updatedFavorites);
+        localStorage.setItem('recipes', JSON.stringify(updatedFavorites));
+    };
+
+    const isFavoriteMap = Object.fromEntries(favorites.map((favorite) => [favorite.id, true]));
+
+    const numberFavorites = favorites.length;
+
+
+    return {
+        favorites,
+        toogleFavorites,
+        isFavoriteMap,
+        numberFavorites,
+    }
+
+};
+
+export default useFavorites;
