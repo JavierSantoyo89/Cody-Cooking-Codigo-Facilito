c90c6c9172398d8b1785cb1b121943bcc7c1fb11
Javier Santoyo <javier.santoyo@yahoo.com>
2024-04-08 01:33:51 -0600

    feat: :ambulance: Connect Login with firebse/typescript

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-
-import MainWrapper from '../common/MainWrapper';
-import { Title as BaseTitle } from '../common/titles';
-import { 
-  Button as BaseButton, 
-  Link as BaseLink, 
-  Paragraph as BaseParagraph, 
-  Clear 
-} from '../common/ui'
-
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import MainWrapper from "../common/MainWrapper";
+import { Title as BaseTitle } from "../common/titles";
+import {
+  Button as BaseButton,
+  Link as BaseLink,
+  Paragraph as BaseParagraph,
+  Clear,
+} from "../common/ui";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useUserStore } from "../../store/userState";
+import loginWithEmailPassword from "../../functions/loginWithEmailPassword";
+import registerUser from "../../functions/registerUser";
+import { mainStylis } from "styled-components/dist/models/StyleSheetManager";
 const Title = styled(BaseTitle)`
   margin-bottom: 100px;
   text-align: center;
 `;
-
 
 const InputText = styled.input`
   appearance: none;
@@ -23,16 +25,15 @@ const InputText = styled.input`
   background: white;
   border: 1px solid #ccc;
   border-radius: 4px;
-  color: #9A9A9A;
+  color: #9a9a9a;
   font-size: 16px;
   margin-bottom: 30px;
   padding: 8px;
   width: 100%;
-
 `;
 
 const Button = styled(BaseButton)`
-  background: #9A9A9A;
+  background: #9a9a9a;
   color: black;
 `;
 
@@ -45,36 +46,85 @@ const Paragraph = styled(BaseParagraph)`
   margin-bottom: 10px;
 `;
 
-
-const Login = () =>  {
+const Login = () => {
   const [isUser, setIsUser] = useState(true);
-  
+
   const toogleIsUser = () => {
     setIsUser((currentValue: boolean) => !currentValue);
-  }
+  };
 
+  type Inputs = {
+    email: string;
+    password: string;
+    userName?: string;
+    name?: string;
+    userEmail?: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<Inputs>();
+  const {setUser} = useUserStore();
+const {setEmail} = useUserStore()
+
+  const onSubmit: SubmitHandler<Inputs> = (data, event) => {
+    event?.preventDefault();
+    const userName = data.name;
+    const email = data.email;
+    const password = data.password;
+    // console.log("voila", email, password);
+
+    if (!isUser) {
+      console.log("entro a registrar usuario");
+      console.log(data);
+      setUser(userName);
+      setEmail(email)
+      registerUser({ email}, { password })
+    
+    } else {
+      console.log("entro a login");
+      loginWithEmailPassword({ email }, { password });
+    }
+  };
   return (
     <MainWrapper>
       <div>
-        <Title>
-          ¡Bienvenido!
-        </Title>
-        <form>
-          {!isUser && <InputText type="text" name="name" id="name" placeholder='Nombre' />}
-          <InputText type="email" name="email" id="email" placeholder='Email' />
-          <InputText type="password" name="password" id="password" placeholder='Contraseña' />
-
-          {isUser 
-            ? <Paragraph>¿Nuevo en recetas? <Link onClick={toogleIsUser}>Crear cuenta</Link></Paragraph>
-            : <Paragraph>¿Ya tienes cuenta? <Link onClick={toogleIsUser}>Ingresar</Link></Paragraph>
-          }
-
+        <Title>¡Bienvenido!</Title>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {!isUser && <label htmlFor="name">Nombre</label>}
+          {!isUser && (
+            <input id="name" {...register("name", { required: true })} />
+          )}
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", { required: true })}
+          />
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            id="password"
+            {...register("password", { required: true })}
+          />
+          <Button>{isUser ? "Login" : "Crear cuenta"}</Button>
+          {isUser ? (
+            <Paragraph>
+              ¿Nuevo en recetas?{" "}
+              <Link onClick={toogleIsUser}>Crear cuenta</Link>
+            </Paragraph>
+          ) : (
+            <Paragraph>
+              ¿Ya tienes cuenta? <Link onClick={toogleIsUser}>Ingresar</Link>
+            </Paragraph>
+          )}
           <Clear />
-          <Button>{isUser ? 'Login' : 'Crear cuenta'}</Button>
         </form>
       </div>
     </MainWrapper>
   );
-}
+};
 
 export default Login;
