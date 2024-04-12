35ad4dd9b7dec373d44912430babb3f97a882722
Javier Santoyo <javier.santoyo@yahoo.com>
2024-04-11 23:17:58 -0600

    Convert .env

--- src/firebase/credentials.ts
+++ src/firebase/credentials.ts
@@ -5,13 +5,22 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj-bGck8gnvJa3G63v08stCv4lH6OiX80",
-  authDomain: "portfolio-63f7d.firebaseapp.com",
-  projectId: "portfolio-63f7d",
-  storageBucket: "portfolio-63f7d.appspot.com",
-  messagingSenderId: "959063440093",
-  appId: "1:959063440093:web:079cf6796776d47f9f3934",
-  measurementId: "G-ZBKTVMJFNW"
+  apiKey: import.meta.env.apikey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
+  measurementId: import.meta.env.measurementId
+  
+  // apiKey: "AIzaSyAj-bGck8gnvJa3G63v08stCv4lH6OiX80",
+  // authDomain: "portfolio-63f7d.firebaseapp.com",
+  // projectId: "portfolio-63f7d",
+  // storageBucket: "portfolio-63f7d.appspot.com",
+  // messagingSenderId: "959063440093",
+  // appId: "1:959063440093:web:079cf6796776d47f9f3934",
+  // measurementId: "G-ZBKTVMJFNW"
+
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
