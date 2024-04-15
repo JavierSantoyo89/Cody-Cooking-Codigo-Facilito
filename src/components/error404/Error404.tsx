a0867464c8b3fee10978a336aeaf47b9aaea4be8
vmacedo <mahua.ea@gmail.com>
2024-04-14 21:08:26 -0500

    feat: page 404 was added

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -1,25 +1,75 @@
 import styled from "styled-components";
-import error404 from "./404Background.webp";
+import CodySVG from "../../assets/imgs/Cody.svg";
+import ArrowSVG from "../../assets/icons/ArrowBack.svg";
+import { Link } from "react-router-dom";
 
 export const Error404 = () => {
-  const handleClickBack = () => {
-    window.history.back();
-  };
-
   return (
-    <ErrorBody onClick={handleClickBack} style={{ cursor: "pointer" }}>
-      <Logo src={error404} alt="Logo" />
-    </ErrorBody>
+    <Container>
+      <Title>404</Title>
+      <Description>¡Ooops!</Description>
+      <Image src={CodySVG} alt="Descripción de la imagen" />
+      <Link to="/">
+        <Button>
+          <Icon />
+          Volver al Home
+        </Button>
+      </Link>
+    </Container>
   );
 };
 
-const ErrorBody = styled.div`
+const Container = styled.div`
+  height: 100vh;
   display: flex;
-  overflow-x: hidden;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Logo = styled.img`
-  width: 100vw;
-  height: 100vh;
+const Title = styled.h1`
+  font-size: 84px;
+  margin-bottom: 5px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
-  
+
+const Description = styled.p`
+  font-size: 36px;
+  margin: 0 0 10px 0;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+`;
+
+const Image = styled.img`
+  width: 200px;
+  margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 350px;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #87c159;
+  color: #fff;
+  border: none;
+  border-radius: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #6a9f4d;
+  }
+`;
+const Icon = styled.span`
+  width: 24px; 
+  height: 24px; 
+  background-image: url(${ArrowSVG}); 
+  background-size: contain; 
+  background-repeat: no-repeat; SVG 
+  background-position: center left; 
+  margin-right: 10px;
+  background-color: #fff;
+  border-radius: inherit;
+`;
