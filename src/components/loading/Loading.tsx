ede6929a289204454438c3dbd8a68a499dd85ed2
Agustin Piuca <aguspiucaquevedo@gmail.com>
2024-04-17 00:52:38 -0300

    Estilos de carrusel + custom cursor creado

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,18 +1,25 @@
+import styled from 'styled-components'
 
-import Style from './loading.module.css'
+
 export const Loading = () => {
   return (
+    <LoadingTitle>Cargando...</LoadingTitle>
     
-    <div className={Style.loader}>
-      <div className={Style.panWrapper}>
-        <div className={Style.pan}>
-          <p>Cargando...</p>
-          <div className={Style.food}></div>
-          <div className={Style.panBase}></div>
-          <div className={Style.panHandle}></div>
-        </div>
-        <div className={Style.panShadow}></div>
-      </div>
-    </div>
+    // <div className={Style.loader}>
+    //   <div className={Style.panWrapper}>
+    //     <div className={Style.pan}>
+    //       <div className={Style.food}></div>
+    //       <div className={Style.panBase}></div>
+    //       <div className={Style.panHandle}></div>
+    //     </div>
+    //     <div className={Style.panShadow}></div>
+    //   </div>
+    // </div>
   )
 }
+
+const LoadingTitle = styled.h1`
+  color: #2b2b2b;
+  font-size: 90px;
+  text-align: center;
+`
