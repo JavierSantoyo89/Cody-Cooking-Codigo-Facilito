638b4f82c2fd1d0e2f41d6d6bae6c53a29577665
Jose Montenegro <grafijoss@hotmail.com>
2024-04-17 23:24:13 -0500

    ingredients added

--- src/views/detail/Detail.tsx
+++ src/views/detail/Detail.tsx
@@ -17,6 +17,7 @@ const recipesHardCode = [
         image: "https://www.themealdb.com/images/media/meals/tqtywx1468317395.jpg",
         title: "Chocolate Gateau",
         video: "https://www.youtube.com/watch?v=dsJtgmAhFF4",
+        ingredients: [],
     }, 
     {
         description: "Cut each chicken breast in about 3 pieces, so that it cooks faster and put it in a small pot. Pour Enchilada sauce over it and cook covered on low to medium heat until chicken is cooked through, about 20 minutes. No water is needed, the chicken will cook in the Enchilada sauce. Make sure you stir occasionally so that it doesn't stick to the bottom.\r\nRemove chicken from the pot and shred with two forks.\r\nPreheat oven to 375 F degrees.\r\nStart layering the casserole. Start with about ¼ cup of the leftover Enchilada sauce over the bottom of a baking dish. I used a longer baking dish, so that I can put 2 corn tortillas across. Place 2 tortillas on the bottom, top with ⅓ of the chicken and ⅓ of the remaining sauce. Sprinkle with ⅓ of the cheese and repeat starting with 2 more tortillas, then chicken, sauce, cheese. Repeat with last layer with the remaining ingredients, tortillas, chicken, sauce and cheese.\r\nBake for 20 to 30 minutes uncovered, until bubbly and cheese has melted and started to brown on top.\r\nServe warm.",
@@ -24,6 +25,7 @@ const recipesHardCode = [
         image: "https://www.themealdb.com/images/media/meals/qtuwxu1468233098.jpg",
         title: "Chicken Enchilada Casserole",
         video: "https://www.youtube.com/watch?v=EtVkwVKLc_M",
+        ingredients: [],
     }, 
     {
         description: "Crust: make a dough from 250g flour (I like mixing different flours like plain and wholegrain spelt flour), 125g butter, 1 egg and a pinch of salt, press it into a tart form and place it in the fridge. Filling: stir 300g cream cheese and 100ml milk until smooth, add in 3 eggs, 100g grated parmesan cheese and season with salt, pepper and nutmeg. Take the crust out of the fridge and prick the bottom with a fork. Pour in the filling and bake at 175 degrees C for about 25 minutes. Cover the tart with some aluminium foil after half the time. In the mean time, slice about 350g mini tomatoes. In a small pan heat 3tbsp olive oil, 3tbsp white vinegar, 1 tbsp honey, salt and pepper and combine well. Pour over the tomato slices and mix well. With a spoon, place the tomato slices on the tart, avoiding too much liquid on it. Decorate with basil leaves and enjoy",
@@ -31,6 +33,7 @@ const recipesHardCode = [
         image: "https://www.themealdb.com/images/media/meals/wurrux1468416624.jpg",
         title: "Cream Cheese Tart",
         video: "https://www.youtube.com/watch?v=UhQPwO4uymo",
+        ingredients: [],
     }
 ]
 
